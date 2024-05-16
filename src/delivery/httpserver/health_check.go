51939e63c963b9cbdf3613179e75bc3bdc376f59
Mohsen Haghgoo Darya Kenari <mohsen.hag@mtnirancell.ir>
2024-05-16 11:36:42 +0330

    Add healthcheck

---
+++ src/delivery/httpserver/health_check.go
@@ -0,0 +1,12 @@
+package httpserver
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (s Server) healthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "everything is good!",
+	})
+}
