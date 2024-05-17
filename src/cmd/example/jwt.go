f197508d7ff24106a088a2e954da093b73e8282b
Mohsen Haghgoo Darya Kenari <mohsen.hag@mtnirancell.ir>
2024-05-17 15:25:14 +0330

    Update jwt example

---
+++ src/cmd/example/jwt.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/mohsenHa/messenger/service/authservice"
+	"os"
+	"time"
+)
+
+func main() {
+
+	claims := authservice.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 10)),
+		},
+		Id: "1234",
+	}
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodRS512, claims)
+	keyDataPrivate, err := os.ReadFile("./key/key")
+	if err != nil {
+		panic(err)
+	}
+	keyprivate, err := jwt.ParseRSAPrivateKeyFromPEM(keyDataPrivate)
+	if err != nil {
+		panic(err)
+	}
+	tokenString, err := accessToken.SignedString(keyprivate)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(tokenString)
+
+	token, err := jwt.ParseWithClaims(tokenString, &authservice.Claims{}, func(token *jwt.Token) (interface{}, error) {
+		keyData, err := os.ReadFile("./key/key.pub")
+		if err != nil {
+			panic(err)
+		}
+		key, err := jwt.ParseRSAPublicKeyFromPEM(keyData)
+		if err != nil {
+			panic(err)
+		}
+		return key, nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	if claims, ok := token.Claims.(*authservice.Claims); ok && token.Valid {
+		fmt.Println(claims)
+	} else {
+		panic(err)
+	}
+
+}
